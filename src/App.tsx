1f866a0b4d27fd5fdc8e9af3eed4b5b2d8713267
Stanislav Otmarshtein <stanislavshina@yandex.ru>
2023-04-05 19:20:34 +0500

    осталось доделать рейтинг

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import { Tabs } from 'antd';
+import type { TabsProps } from 'antd';
 
 import SearchTab from './components/SearchTab/SearchTab';
 import './App.scss';
 
 function App() {
+  const moviesLists: TabsProps['items'] = [
+    {
+      key: '1',
+      label: 'Search',
+      children: <SearchTab />
+    },
+    {
+      key: '2',
+      label: 'Rated',
+      children: 'Rated movies will here'
+    }
+  ]
   return (
     <div className="App">
-      <SearchTab />
+      <Tabs defaultActiveKey='1' items={moviesLists} centered={true}/>
     </div>
   );
 }
